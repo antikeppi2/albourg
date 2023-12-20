4f0ffaa4f90d18adb86f99a4e9d664a07c25d1b9
antikeppi2 <79043941+antikeppi2@users.noreply.github.com>
2023-12-20 16:22:33 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>REGISTER</title>
+  
+
+</head>
+<body>
+Minecraft IGN (In Game Name): <input type="text" name="username">
+<br />Password:<input type="password" name="username">
+<br />Confirm Password:<input type="password" name="username">
+<br /><input type="submit" name="submit" value="Register">
+</body>
+</html>
