2b7cec7d01f00662a94b0271734191a4122b0df7
antikeppi2 <79043941+antikeppi2@users.noreply.github.com>
2023-12-21 18:42:26 +0100

    Update register.php

--- register.php
+++ register.php
@@ -5,9 +5,20 @@
 
 </head>
 <body>
-Minecraft IGN (In Game Name): <input type="text" name="username">
-<br />Password:<input type="password" name="username">
-<br />Confirm Password:<input type="password" name="username">
-<br /><input type="submit" name="submit" value="Register">
+  <form action="register.php" method="POST">
+    Minecraft IGN (In Game Name): <input type="text" name="username">
+    <br />Password:<input type="password" name="password">
+    <br />Confirm Password:<input type="password" name="repassword">
+    <br /><input type="submit" name="submit" value="Register"> or <a href="login.php">Login</a>
 </body>
 </html>
+<?php
+$username = @$_POST['username'];
+$username = @$_POST['password'];
+$username = @$_POST['repassword'];
+
+if(isset($_POST['submit'])){
+  echo "Username - ".$username"
+  echo "Passoword - ".$password"
+  echo "Repassword - ".$repassword"
+?>
