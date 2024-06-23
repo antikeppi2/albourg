a0e51bfe77cb4f861e4d1bb234dbdc0304664cb9
antikeppi2 <79043941+antikeppi2@users.noreply.github.com>
2024-06-23 10:44:10 +0200

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+      <meta charset="utf-8">
+      <title>Registration</title>
+  </head>
+  <body>
+    <h2>Registration</h2>
+    <form class="" actions"" ></form>
+  </body>
