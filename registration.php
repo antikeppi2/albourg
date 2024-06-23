f85af7b129aa7e4fff96c0f86ba3619762392d6e
antikeppi2 <79043941+antikeppi2@users.noreply.github.com>
2024-06-23 10:51:06 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,3 +1,8 @@
+<?php
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -6,5 +11,15 @@
   </head>
   <body>
     <h2>Registration</h2>
-    <form class="" actions"" ></form>
+    <form class="" actions="" method="post" autocomplete="off">
+      <label for="username">Username : </label>
+      <input type="text" name="username" id = "username" required value=""> <br>
+      <label for="password">Password : </label>
+      <input type="password" name="password" id = "password" required value=""> <br>
+      <label for="confirmpassword">Confirm Password : </label>
+      <input type="password" name="confirmpassword" id = "confirmpassword" required value=""> <br>
+      <button type="submit" name="submit">Register</button>
+    </form>
+    <br>
+    <a href="login">Login</a>
   </body>
