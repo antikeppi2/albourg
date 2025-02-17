8bd3140c10f5779cc11b2f735e5297eaf5de9db1
antikeppi2 <79043941+antikeppi2@users.noreply.github.com>
2025-02-17 16:14:25 +0100

    Update userregister.js

--- userregister.js
+++ userregister.js
@@ -1,25 +1,59 @@
-fetchData();
+const baseUrl = "https://minecraftonline.com/cgi-bin/";
 
-async function fetchData(){
+async function getUserInfo(user) {
+  const firstSeenUrl = `${baseUrl}/getfirstseen?${user}`;
+  const lastSeenUrl = `${baseUrl}/getlastseen?${user}`;
+  const timeOnlineUrl = `${baseUrl}/gettimeonline?${user}`;
+  const isPlayerBannedUrl = `${baseUrl}/getbanwhy.sh?${user}`;
+  const usernameUrl = `${baseUrl}/getcorrectname?${user}`;
 
-  try{
-
-    const userName = document.getElementById("userName").value;
-    const responde = await fetch("https://minecraftonline.com/cgi-bin/getfirstseen?${}");
-
-    if(!response.ok){
-      throw new Error("Failed To Retrieve Data");
-      
+  try {
+    const usernameResponse = await fetch(usernameUrl);
+    if (usernameResponse.ok) {
+      const username = await usernameResponse.text();
+      console.log(`Username:\n${username}`);
+    } else {
+      console.log(`Failed To Retrieve Data ${usernameResponse.status}`);
     }
 
-    const data = await response.text();
-    const userHead = data.sprites.front_default;
-    const imgElement = document.getElementById("userHead");
+    const firstSeenResponse = await fetch(firstSeenUrl);
+    if (firstSeenResponse.ok) {
+      const firstSeen = await firstSeenResponse.text();
+      console.log(`First Seen:\n${firstSeen}`);
+    } else {
+      console.log(`Failed To Retrieve Data ${firstSeenResponse.status}`);
+    }
 
-    imgElement.src = "https://minecraftonline.com/cgi-bin/getplayerhead.sh?${}";
-    imgElement.style.display = "block";
-  }
-  catch(error){
-    console.error(error);
+    const lastSeenResponse = await fetch(lastSeenUrl);
+    if (lastSeenResponse.ok) {
+      const lastSeen = await lastSeenResponse.text();
+      console.log(`Last Seen:\n${lastSeen}`);
+    } else {
+      console.log(`Failed To Retrieve Data ${lastSeenResponse.status}`);
+    }
+
+    const timeOnlineResponse = await fetch(timeOnlineUrl);
+    if (timeOnlineResponse.ok) {
+      const seconds = await timeOnlineResponse.text();
+      const cleanSeconds = seconds.trim();
+      const time = new Date(0);
+      time.setSeconds(parseInt(cleanSeconds));
+      console.log(`Time Played:\n${new Date(time).toISOString().substr(11, 8)}`);
+    } else {
+      console.log(`Failed To Retrieve Data ${timeOnlineResponse.status}`);
+    }
+
+    const isPlayerBannedResponse = await fetch(isPlayerBannedUrl);
+    if (isPlayerBannedResponse.ok) {
+      const isBanned = await isPlayerBannedResponse.text();
+      console.log(`Is Player Banned?\n${isBanned}`);
+    } else {
+      console.log(`Failed To Retrieve Data ${isPlayerBannedResponse.status}`);
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
   }
 }
+
+const userName = "Legnano";
+getUserInfo(userName);
