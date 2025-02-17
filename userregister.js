43ef88032c4f99d5bcc748cf8664b378ede9cc91
antikeppi2 <79043941+antikeppi2@users.noreply.github.com>
2025-02-17 17:11:21 +0100

    Update userregister.js

--- userregister.js
+++ userregister.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const corsProxy = 'https://api.allorigins.win/get?url=';  // CORS proxy URL
+  const corsProxy = 'https://cors-proxy.htmldriven.com/?url=';  // CORS proxy URL
   const baseUrl = "https://minecraftonline.com/cgi-bin/";
 
   async function getUserInfo(user) {
